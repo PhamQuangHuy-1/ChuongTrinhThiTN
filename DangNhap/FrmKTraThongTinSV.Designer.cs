4272622fd30b169360981e1960cf85373f1acf4e
Dang Minh Hoang <11201581@st.neu.edu.vn>
2022-06-26 15:45:49 +0700

    3h45 26/6

--- DangNhap/FrmKTraThongTinSV.Designer.cs
+++ DangNhap/FrmKTraThongTinSV.Designer.cs
@@ -36,15 +36,17 @@ namespace DangNhap
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 18);
+            this.label1.Location = new System.Drawing.Point(33, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 36);
+            this.label1.Size = new System.Drawing.Size(181, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Họ và tên:";
             // 
@@ -52,9 +54,9 @@ namespace DangNhap
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 70);
+            this.label2.Location = new System.Drawing.Point(33, 88);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(191, 36);
+            this.label2.Size = new System.Drawing.Size(229, 40);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mã sinh viên:";
             // 
@@ -62,9 +64,9 @@ namespace DangNhap
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(29, 122);
+            this.label3.Location = new System.Drawing.Point(33, 152);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(157, 36);
+            this.label3.Size = new System.Drawing.Size(187, 40);
             this.label3.TabIndex = 2;
             this.label3.Text = "Ngày sinh:";
             // 
@@ -72,10 +74,10 @@ namespace DangNhap
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(240, 171);
+            this.button1.Location = new System.Drawing.Point(290, 214);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(253, 43);
+            this.button1.Size = new System.Drawing.Size(183, 54);
             this.button1.TabIndex = 3;
             this.button1.Text = "Vào Thi";
             this.button1.UseVisualStyleBackColor = false;
@@ -85,9 +87,9 @@ namespace DangNhap
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(196, 18);
+            this.label4.Location = new System.Drawing.Point(220, 22);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 36);
+            this.label4.Size = new System.Drawing.Size(47, 40);
             this.label4.TabIndex = 5;
             this.label4.Text = "ht";
             // 
@@ -95,9 +97,9 @@ namespace DangNhap
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(238, 70);
+            this.label5.Location = new System.Drawing.Point(268, 88);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 36);
+            this.label5.Size = new System.Drawing.Size(83, 40);
             this.label5.TabIndex = 6;
             this.label5.Text = "msv";
             // 
@@ -105,17 +107,43 @@ namespace DangNhap
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(201, 122);
+            this.label6.Location = new System.Drawing.Point(226, 152);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 36);
+            this.label6.Size = new System.Drawing.Size(55, 40);
             this.label6.TabIndex = 7;
             this.label6.Text = "ns";
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(40, 214);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 54);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Đăng nhập lại";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(525, 214);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(223, 54);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
             // FrmKTraThongTinSV
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(697, 246);
+            this.ClientSize = new System.Drawing.Size(784, 308);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -141,5 +169,7 @@ namespace DangNhap
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
